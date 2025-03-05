95f3bf3a377666c7f3c66e1982e5b438565852a1
Allie Bastien <alliebastien11@gmail.com>
2025-03-05 14:23:55 -0500

    Ajout d'un systeme d'envoi du formulaire par courriel

--- action/formAction.js
+++ action/formAction.js
@@ -1,7 +1,10 @@
 "use server";
+import sendEmail from "@/sendEmail";
 
 export async function submitForm(prevState, formData) {
   let errors = {};
+
+  
 
   if (!formData.get("name")) {
     errors.name = "Le nom est requis.";
@@ -19,7 +22,17 @@ export async function submitForm(prevState, formData) {
     return errors; // Retourne les erreurs sans recharger la page
   }
 
-  console.log("Données soumises :", Object.fromEntries(formData));
+  let nom = Object.fromEntries(formData).name
+  let email = Object.fromEntries(formData).email
+  let message = Object.fromEntries(formData).message
+
+  sendEmail({
+    subject: 'Message automatiée envoyé depuis la page contact ',
+    text: "Ceci est un message automatisé\n\nUn utilisateur à envoyé un message depuis le formulaire de contact, voici les informations relatives au message :\nNom de l'utilisateur : " + nom 
+    + "\nAddresse courriel de l'utilisateur : " + email + "\n\nContenu du message : " + message + "\n\n\n-Système d'envoi de message, Thrive Today.",
+    to: "ThriveTodayContact@gmail.com",
+    from: process.env.EMAIL
+  });
 
   return { success: true };
 }
